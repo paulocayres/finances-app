8c7d59f60213b701a659c90636cb2300e4e2e5f6
paulocayres <paulo.cayres@gmail.com>
2025-05-21 16:31:26 -0300

    Reinicializando o repositório remoto com nova versão local

--- src/app/pages/login/login.page.ts
+++ src/app/pages/login/login.page.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { AuthService } from '../../services/auth.service.service';
+AuthService
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
+  standalone: false
 })
-export class LoginPage implements OnInit {
+export class LoginPage {
+  constructor(private auth: AuthService, private router: Router) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  async login() {
+    try {
+      await this.auth.loginWithGoogle();
+      this.router.navigateByUrl('/agenda-mensal', { replaceUrl: true });
+    } catch (err) {
+      console.error('Erro no login:', err);
+    }
   }
-
 }
--- src/app/services/auth.service.service.ts
+++ src/app/services/auth.service.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import { initializeApp } from 'firebase/app';
+import {
+  getAuth,
+  GoogleAuthProvider,
+  signInWithPopup,
+  signOut,
+  onAuthStateChanged,
+  User
+} from 'firebase/auth';
 
-@Injectable({
-  providedIn: 'root'
-})
-export class AuthServiceService {
+const firebaseConfig = {
+  apiKey: "AIzaSyAK1jVnCE6c2-l1WIsZTvVWN3xeruN0a5o",
+  authDomain: "cayresfinances-app.firebaseapp.com",
+  projectId: "cayresfinances-app",
+  storageBucket: "cayresfinances-app.firebasestorage.app",
+  messagingSenderId: "332663746330",
+  appId: "1:332663746330:web:a6759fcf314ed4122d86a3",
+  measurementId: "G-PE1ZH43L5N"
+};
 
-  constructor() { }
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+
+@Injectable({ providedIn: 'root' })
+export class AuthService {
+  user: User | null = null;
+
+  constructor() {
+    onAuthStateChanged(auth, (user) => {
+      this.user = user;
+    });
+  }
+
+  loginWithGoogle() {
+    const provider = new GoogleAuthProvider();
+    return signInWithPopup(auth, provider);
+  }
+
+  logout() {
+    return signOut(auth);
+  }
+
+  getCurrentUser() {
+    return auth.currentUser;
+  }
 }
