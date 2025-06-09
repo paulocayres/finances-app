ecf603627765ce6cf85c3e46e31eb157cf592636
paulocayres <paulo.cayres@gmail.com>
2025-06-09 20:45:04 -0300

    inicio da branch

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.cayres.financesapp',
-  appName: 'finances-app',
+  appName: 'Cayres Finances',
   webDir: 'www',
 
   plugins: {
